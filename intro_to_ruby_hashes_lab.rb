1a4b268087e8136d24fe05772341fa9864bb39b0
Samuel Escobar <sescobar8714@gmail.com>
2020-01-29 00:07:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 		{:reading_railroad => {},
 		:pennsylvania_railroad => {},
 		:b_and_o_railroad => {},
-		:shortline_railroad => {} 
+		:shortline_railroad => {}
 	}
     {:pieces => 4}
 	    {:rent_in_dollars =>
