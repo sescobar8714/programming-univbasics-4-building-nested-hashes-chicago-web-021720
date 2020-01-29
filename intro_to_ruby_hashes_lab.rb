cba4305de59989078dfe315268820cf038531b43
Samuel Escobar <sescobar8714@gmail.com>
2020-01-29 00:40:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,13 +25,13 @@ def monopoly_with_third_tier
   # complexity.
 	base_hash = {
 		:railroads =>
-		{:pieces => 4}
-		:names =>{
+	{:pieces => 4,
+	:names =>{
 		:reading_railroad => {},
 		:pennsylvania_railroad => {},
 		:b_and_o_railroad => {},
-		:shortline_railroad => {}
-	    {:rent_in_dollars =>
+		:shortline_railroad => {}},
+	 :rent_in_dollars =>{
 			 :one_piece_owned => "25",
 			 :two_pieces_owned => "50",
 			 :three_pieces_owned => "100",
